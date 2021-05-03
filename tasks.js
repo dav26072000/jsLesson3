065eff7916ea45e4045adefa1ac19bbf8e526992
David Matiashvili <70051407+dav26072000@users.noreply.github.com>
2021-05-03 17:16:17 +0400

    build[in parseIntFunction]: end parseInt task

--- tasks.js
+++ tasks.js
@@ -1,4 +1,5 @@
 //  What will be the result and why?
+
 // "" + 1 + 0     => "10" | string + number = string
 // "" - 1 + 0     => NaN | string - number is not a number => NaN
 // true + false   => true
@@ -13,3 +14,51 @@
 // " -9 " - 5     => -14
 // null + 1       => 1
 // undefined + 1  => NaN
+
+//Write logic that implements parseInt
+function parseIntFunction(str, base) {
+  str = str.split("");
+  let newNum = [];
+  let zeroInStart = false;
+  if (str[0] === "-") {
+    newNum.push("-");
+  }
+  for (let i = 0; i < str.length; i++) {
+    if (
+      !Number(str[i]) &&
+      zeroInStart === false &&
+      str[i] != "-" &&
+      str[i] != "0"
+    ) {
+      return NaN;
+    } else {
+      if (
+        typeof base !== "undefined" &&
+        !Number(str[i]) &&
+        zeroInStart === false
+      ) {
+      } else if (
+        (str[i] === "0" && zeroInStart === false) ||
+        (!Number(str[i]) && zeroInStart === false)
+      ) {
+        continue;
+      }
+      if (!Number(str[i]) && Number(str[i]) !== 0) {
+        break;
+      }
+      newNum.push(str[i]);
+      zeroInStart = true;
+    }
+  }
+  if (typeof base !== "undefined") {
+    let result = 0;
+    for (let j = 0; j < newNum.length; j++) {
+      result += Number(newNum[newNum.length - j - 1]) * Math.pow(base, j);
+    }
+    return result;
+  } else {
+    return newNum.join("");
+  }
+}
+
+console.log(parseIntFunction("011110", 2));
